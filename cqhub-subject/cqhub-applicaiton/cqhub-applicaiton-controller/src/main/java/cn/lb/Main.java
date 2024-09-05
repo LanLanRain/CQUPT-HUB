0f6faa01641166c54238320521ac0070ff4923c9
LunerBreeze <RainSoul1120@outlook.com>
2024-09-05 13:54:12 +0800

    项目初始化工程

---
+++ cqhub-subject/cqhub-applicaiton/cqhub-applicaiton-controller/src/main/java/cn/lb/Main.java
@@ -0,0 +1,11 @@
+package cn.lb;
+
+/**
+ * @author RainSoul
+ * @create 2024-09-05
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
